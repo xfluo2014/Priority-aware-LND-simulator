e7fd56107cbcaf2430ac3056e10fa9cb849d5654
xfluo2014 <50399173+xfluo2014@users.noreply.github.com>
2021-02-16 22:46:43 +0900

    Update p_peers.py

--- p_peers.py
+++ p_peers.py
@@ -6,7 +6,7 @@ from datetime import timedelta
 from p_mailbox import Mailbox
 from payments import payment
 from a_core import Pay_agent
-import random
+import scipy.stats as ss
 import copy,gc
 
 lock = Lock()
