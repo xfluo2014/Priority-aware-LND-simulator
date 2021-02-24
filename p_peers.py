614f785d81c8c421296bfd6c950430e4af6fd394
xfluo2014 <50399173+xfluo2014@users.noreply.github.com>
2021-02-24 11:14:02 +0900

    Update p_peers.py

--- p_peers.py
+++ p_peers.py
@@ -304,5 +304,5 @@ class peer:
 		xU, xL = x + 0.5, x - 0.5
 		prob = ss.norm.cdf(xU, scale = 3) - ss.norm.cdf(xL, scale = 3)
 		prob = prob / prob.sum() #normalize the probabilities so their sum is 1
-		nums, = np.random.choice(x, size = var_size, p = prob)
+		nums, = np.random.RandomState().choice(x, size = var_size, p = prob)
 		return nums
